b408fcad1d2a3e1bc1c760ada71db346ecf075fd
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2023-02-03 00:52:01 -0300

    Added requirements, Comments, and some functions
    I've added the requirements.txt file and comments I've forgotten to add. Additionally, I've created two functions to make the code shorter and easier to read

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import sys
 
+# Importing classes from PySide6 modules
 from PySide6.QtCore import QDate
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QApplication, QMainWindow
+
+# Importing user interface file
 from ui_main import Ui_MainWindow
+
+# Importing SQLite3 module for database operations
 import sqlite3
+
 import datetime
 
 # Creating the class for the main window
@@ -14,27 +20,44 @@ class Window(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.create_table()
+
+        # Connecting "Add" button to the add_user method
         self.ui.AddButton.clicked.connect(self.add_user)
+
+        # Loading data from the database to the table widget
         self.load_data()
+
+        # Connecting table item selection to the load_user method
         self.ui.SQL_Table.clicked.connect(self.load_user)
+
+        # Connecting "Disconnect" button to the delete_user method
         self.ui.DisconnectButton.clicked.connect(self.delete_user)
+
+        # Connecting "Update" button to the update_user method
         self.ui.UpdateButton.clicked.connect(self.update_user)
+
+        # Connecting text change in the search entry to the search_in_table method
         self.ui.SearchEntry.textChanged.connect(self.search_in_table)
 
-
+    # Method to create the table in the database
     def create_table(self):
         try:
+            # Connecting to the database
             conn = sqlite3.connect("database.db")
             c = conn.cursor()
             c.execute("CREATE TABLE IF NOT EXISTS users (Name TEXT, Sex TEXT, Birthday TEXT, Salary TEXT, Date TEXT)")
             conn.commit()
             conn.close()
+
+            # Setting the status line text to "Connected"
             self.ui.StatusLine.setText("<html><head/><body><p><span style=\" font-size:14pt; font-weight:700; color:#ffffff;\">Status: Connected</span></p></body></html>")
         except:
+            # Setting the status line text to "Disconnected" if an exception occurs
             self.ui.StatusLine.setText("<html><head/><body><p><span style=\" font-size:14pt; font-weight:700; color:#ffffff;\">Status: Disconnected</span></p></body></html>")
 
+    # Method to load data from the database to the table widget
     def load_data(self):
-        # clear existing items in the table widget
+        # Clear any existing items in the table widget
         self.ui.SQL_Table.clearContents()
 
         conn = sqlite3.connect("database.db")
@@ -51,7 +74,15 @@ class Window(QMainWindow):
             self.ui.SQL_Table.setItem(tablerow, 3, QtWidgets.QTableWidgetItem(row[3]))
             self.ui.SQL_Table.setItem(tablerow, 4, QtWidgets.QTableWidgetItem(row[4]))
             tablerow+=1
+    
+    # Method to erase the user information to the Qt entries
+    def erase_entries(self):
+        self.ui.NameEntry.setText("")
+        self.ui.SexOption.setCurrentIndex(0)
+        self.ui.BirthDayEntry.setDate(QDate(2000, 1, 1))
+        self.ui.SalaryEntry.setText("")
 
+    # Method to add a user to the database and table widget
     def add_user(self):
         name = str(self.ui.NameEntry.text()).capitalize()
         sex = str(self.ui.SexOption.currentText())
@@ -64,12 +95,10 @@ class Window(QMainWindow):
         conn.commit()
         conn.close()
         print("User Added")
-        self.ui.NameEntry.setText("")
-        self.ui.SexOption.setCurrentIndex(0)
-        self.ui.BirthDayEntry.setDate(QDate(2000, 1, 1))
-        self.ui.SalaryEntry.setText("")
+        self.erase_entries()
         self.load_data()
 
+    # Method to load the user information to the Qt entries
     def load_user(self, index):
         self.row = index.row()
         try:
@@ -85,11 +114,9 @@ class Window(QMainWindow):
             self.ui.BirthDayEntry.setDate(birthday)
             self.ui.SalaryEntry.setText(salary)
         except:
-            self.ui.NameEntry.setText("")
-            self.ui.SexOption.setCurrentIndex(0)
-            self.ui.BirthDayEntry.setDate(QDate(2000, 1, 1))
-            self.ui.SalaryEntry.setText("")
+            self.erase_entries()
 
+    # Method to delete a user from the database and table widget
     def delete_user(self, index):
         row = self.row
         conn = sqlite3.connect("database.db")
@@ -116,11 +143,9 @@ class Window(QMainWindow):
         conn.commit()
         conn.close()
 
-        self.ui.NameEntry.setText("")
-        self.ui.SexOption.setCurrentIndex(0)
-        self.ui.BirthDayEntry.setDate(QDate(2000, 1, 1))
-        self.ui.SalaryEntry.setText("")
+        self.erase_entries()
 
+    # Method to update a user to the database and table widget
     def update_user(self, index):
         row = self.row
         conn = sqlite3.connect("database.db")
@@ -134,12 +159,10 @@ class Window(QMainWindow):
         conn.commit()
         conn.close()
         self.load_data()
-        self.ui.NameEntry.setText("")
-        self.ui.SexOption.setCurrentIndex(0)
-        self.ui.BirthDayEntry.setDate(QDate(2000, 1, 1))
-        self.ui.SalaryEntry.setText("")
+        self.erase_entries()
         self.load_data()
 
+    # Method to search a user to the database and table widget
     def search_in_table(self, text):
         for row in range(self.ui.SQL_Table.rowCount()):
             for col in range(self.ui.SQL_Table.columnCount()):
@@ -147,6 +170,8 @@ class Window(QMainWindow):
                 if item and text in item.text():
                     self.ui.SQL_Table.scrollToItem(item)
                     break
+
+
 
 # Creating the main window and showing it
 app = QApplication(sys.argv)
